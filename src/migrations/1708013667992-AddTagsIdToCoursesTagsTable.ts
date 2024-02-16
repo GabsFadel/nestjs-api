adf74144bb4ad3adc5e186e6cda6478f48c103f2
Gabriel Fadel <theghost947@gmail.com>
2024-02-16 15:52:24 -0300

    typeorm creating tables

---
+++ src/migrations/1708013667992-AddTagsIdToCoursesTagsTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddTagsIdToCoursesTagsTable1708013667992 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
